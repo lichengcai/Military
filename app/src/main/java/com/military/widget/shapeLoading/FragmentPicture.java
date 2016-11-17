90e9688de4a371926fe06bd0333e6e1f9aa32f13
lichengcai <lichengcai@koolearn.com>
2016-11-17 18:28:46 +0800

    fragment add

---
+++ app/src/main/java/com/military/widget/shapeLoading/FragmentPicture.java
@@ -0,0 +1,28 @@
+package com.military.widget.shapeLoading;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.military.R;
+
+/**
+ * Created by lichengcai on 2016/11/17.
+ */
+
+public class FragmentPicture extends FragmentBase {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_picture,container,false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
