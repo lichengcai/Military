56379718e1e5a9205c8674437c58428a88df91f5
lichengcai <lichengcai@koolearn.com>
2016-11-17 15:53:32 +0800

    Jsoup selector

--- app/src/main/java/com/military/MainActivity.java
+++ app/src/main/java/com/military/MainActivity.java
@@ -1,69 +1,12 @@
 package com.military;
 
 import android.app.Activity;
-import android.os.Handler;
-import android.os.Message;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
 
 public class MainActivity extends Activity {
-    TextView textView;
-    private Handler handler=new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case 1:
-                    StringBuilder stringBuilder = new StringBuilder();
-
-                    Document document = (Document) msg.obj;
-
-                    Elements links = document.select("a[href]");
-                    for (Element link : links) {
-                        String linkHref = link.attr("href");
-                        stringBuilder.append(linkHref).append("\n");
-                        Log.d("link"," linkHref--" + linkHref);
-                    }
-                    textView.setText(stringBuilder.toString());
-                    break;
-            }
-        }
-    };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = (TextView) findViewById(R.id.text);
-
-        getData();
     }
-
-    public void getData() {
-        //solo
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //http://movie.douban.com/review/best/?start=0
-                String url = "http://mil.huanqiu.com/";
-                try {
-                    Document document = Jsoup.connect(url).get();
-                    handler.obtainMessage(1,document).sendToTarget();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
 }
--- app/src/main/java/com/military/TestActivity.java
+++ app/src/main/java/com/military/TestActivity.java
@@ -5,34 +5,76 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 
-import java.io.File;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 /**
  * Created by lichengcai on 2016/11/16.
  */
 
 public class TestActivity extends Activity {
-    private VideoView imageView;
+    TextView textView;
+    private Handler handler=new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    StringBuilder stringBuilder = new StringBuilder();
+                    Document document = (Document) msg.obj;
+                    Elements links = document.select("div.newsFir");
+                    Log.d("links"," links---" + links.size());
+                    for (Element element : links) {
+                        Elements elements = element.select("a");
+                        for (Element element1 : elements) {
+                            Log.d("title","title--" + element1.attr("title"));
+                            stringBuilder.append(element1.attr("title")).append("\n");
+                        }
+
+                    }
+                    textView.setText(stringBuilder.toString());
+                    break;
+            }
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        imageView = (VideoView) findViewById(R.id.videoView);
 
-        imageView.setVideoURI(Uri.fromFile(new File("/sdcard/cc00.mp4")));
-        imageView.start();
-        imageView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+        textView = (TextView) findViewById(R.id.text);
+
+        getData();
+    }
+
+    public void getData() {
+        //solo
+        new Thread(new Runnable() {
             @Override
-            public void onCompletion(MediaPlayer mp) {
-                Toast.makeText(TestActivity.this,"video is finish",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(TestActivity.this,MainActivity.class));
-                finish();
+            public void run() {
+                //http://movie.douban.com/review/best/?start=0
+                String url = "http://mil.huanqiu.com/";
+                try {
+                    Document document = Jsoup.connect(url).get();
+                    handler.obtainMessage(1,document).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        });
-
+        }).start();
     }
 }
