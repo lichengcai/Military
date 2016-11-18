7455b34f0a23dcfceb530e29b087fce318a01c4b
lichengcai <lichengcai@koolearn.com>
2016-11-18 10:54:56 +0800

    banner

--- app/src/main/java/com/military/MainActivity.java
+++ app/src/main/java/com/military/MainActivity.java
@@ -1,11 +1,16 @@
 package com.military;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        Glide.with(this).load("http://himg2.huanqiu.com/attachment2010/2016/1118/08/50/20161118085035979.jpg").into(imageView);
     }
 }
