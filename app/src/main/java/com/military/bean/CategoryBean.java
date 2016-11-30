17fc7243888ba44f7953a066f21d4fb87825ac03
lichengcai <lichengcai@koolearn.com>
2016-11-30 18:35:43 +0800

    WeaponActivity

--- app/src/main/java/com/military/bean/CategoryBean.java
+++ app/src/main/java/com/military/bean/CategoryBean.java
@@ -7,6 +7,39 @@ package com.military.bean;
 public class CategoryBean {
     private String name;
     private String linkUrl;
+    private String imgUrl;
 
 
+    @Override
+    public String toString() {
+        return "CategoryBean{" +
+                "name='" + name + '\'' +
+                ", linkUrl='" + linkUrl + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLinkUrl() {
+        return linkUrl;
+    }
+
+    public void setLinkUrl(String linkUrl) {
+        this.linkUrl = linkUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
 }
--- app/src/main/java/com/military/huanqiu/weapon/WeaponActivity.java
+++ app/src/main/java/com/military/huanqiu/weapon/WeaponActivity.java
@@ -2,20 +2,30 @@ package com.military.huanqiu.weapon;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ExpandableListView;
 
 import com.military.R;
 import com.military.ui.activity.BaseActivity;
 
-import in.srain.cube.views.ptr.header.StoreHouseHeader;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by lichengcai on 2016/11/29.
  */
 
 public class WeaponActivity extends BaseActivity {
+    @BindView(R.id.list_category)
+    ExpandableListView mListCategory;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weapon);
+        ButterKnife.bind(this);
+
+
     }
 }
