86d8ae702c842e69e4e64e7615e62e71a2687bd4
lichengcai <lichengcai@koolearn.com>
2016-12-15 16:17:21 +0800

    add tinker

--- app/src/main/java/com/military/MilitaryApplication.java
+++ app/src/main/java/com/military/MilitaryApplication.java
@@ -1,47 +1,78 @@
 package com.military;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.support.multidex.MultiDex;
 
 import com.military.bean.Channel;
+import com.military.common.Constants;
 import com.military.utils.CrashHandler;
 import com.squareup.leakcanary.LeakCanary;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.app.TinkerApplication;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobConfig;
+
 /**
  * Created by lichengcai on 2016/11/18.
  */
 
-public class MilitaryApplication extends Application {
+@DefaultLifeCycle(
+        //这里将com.github.tinkersample.SampleApplicationLike更改为你自己的包名即可
+        application = "com.military.SampleApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL
+)
+public class MilitaryApplication extends DefaultApplicationLike {
     private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
     private static final int CORE_POOL_COUNT = CPU_COUNT+1;
     private static final int MAXNUM_POOL_COUNT = CPU_COUNT*2+1;
     private static final long KEEP_TIME = 10L;
 
-    private static MilitaryApplication mInstance;
     public static final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
     public static ArrayList<Channel> mSelected = new ArrayList<>();
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mInstance = this;
+    public MilitaryApplication(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent, Resources[] resources, ClassLoader[] classLoader, AssetManager[] assetManager) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent, resources, classLoader, assetManager);
+
+        //第二：自v3.4.7版本开始,设置BmobConfig,允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)，
+        BmobConfig config =new BmobConfig.Builder(application)
+                ////设置appkey
+                .setApplicationId(Constants.BMOB_APPLICATION_ID)
+                ////请求超时时间（单位为秒）：默认15s
+                .setConnectTimeout(30)
+                ////文件分片上传时每片的大小（单位字节），默认512*1024
+                .setUploadBlockSize(1024*1024)
+                ////文件的过期时间(单位为秒)：默认1800s
+                .setFileExpiration(2500)
+                .build();
+        Bmob.initialize(config);
 
         CrashHandler crashHandler = CrashHandler.getInstance();
-        crashHandler.init(this);
+        crashHandler.init(application);
 
-        if (LeakCanary.isInAnalyzerProcess(this)) {
+        if (LeakCanary.isInAnalyzerProcess(application)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
             return;
         }
-        LeakCanary.install(this);
+//        LeakCanary.install(application);
     }
 
-    public static MilitaryApplication getInstance() {
-        return mInstance;
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        MultiDex.install(base);
+        TinkerInstaller.install(this);
     }
-
 }
---
+++ app/src/main/java/com/military/common/Constants.java
@@ -0,0 +1,9 @@
+package com.military.common;
+
+/**
+ * Created by lichengcai on 2016/12/15.
+ */
+
+public class Constants {
+    public static final String BMOB_APPLICATION_ID = "1290038d6778787eff9664e6b9468e01";
+}
